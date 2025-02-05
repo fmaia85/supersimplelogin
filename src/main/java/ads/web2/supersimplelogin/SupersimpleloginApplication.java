9e2e74e939ba159e71a00e1471ffd872fb194801
fmaia85 <felipe.maia@ifce.edu.br>
2025-02-05 15:32:36 -0300

    Initial commit

---
+++ src/main/java/ads/web2/supersimplelogin/SupersimpleloginApplication.java
@@ -0,0 +1,13 @@
+package ads.web2.supersimplelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupersimpleloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupersimpleloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ads/web2/supersimplelogin/SupersimpleloginApplicationTests.java
@@ -0,0 +1,13 @@
+package ads.web2.supersimplelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupersimpleloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
